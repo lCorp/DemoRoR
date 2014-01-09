525fafc33f1e3f4db8e53a516930b51eee5c3056
buddyngo <buddyngo@yahoo.com>
2014-01-09 17:00:40 +0700

    Install RSpec for Unit Test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoRoR::Application.config.secret_key_base = '6f53dc3cbc1d8572b9cbddaa2c35d318d786d08940cafe5ec12f9263670351f7021da16cd94116cf2888ffe8c8b6bba71263a320284a21ba887ce71326ddd025'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+DemoRoR::Application.config.secret_key_base = secure_token
